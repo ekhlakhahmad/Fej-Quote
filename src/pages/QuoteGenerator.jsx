0d9e43fd119085cd660d560d2cc2efc96cd5937a
Ekhlakh Ahmad <ekhlakhahmad95@gmail.com>
2024-06-28 17:02:40 +0530

    some images deleted and added something

--- src/pages/QuoteGenerator.jsx
+++ src/pages/QuoteGenerator.jsx
@@ -9,7 +9,6 @@ import chanakya from "../assets/Chanakya_Niti";
 import Mahatma_Gandhi from "../assets/Mahatma_Gandhi";
 import Education from "../assets/Education";
 import Martin_Luther from "../assets/Martin_Luther";
-import border1 from "./border1.jpg";
 
 const QuoteGenerator = () => {
 	const navigate = useNavigate();
@@ -36,9 +35,8 @@ const QuoteGenerator = () => {
 	const { Hiquote, Enquote } = data[params.id][currentIndex];
 
 	return (
-		<div className="relative w-[60%] h-[630px] flex mx-auto">
-			<img className="w-full" src={border1} alt="" />
-			<div className="absolute flex flex-col p-4 gap-5 w-[80%] top-[15em] left-[calc(50%-370px)]">
+		<div className="h-screen flex flex-col justify-center items-center">
+			<div className="w-[90%] max-w-screen-md flex flex-col p-8 gap-5 border-2 border-black rounded-lg dark:text-white dark:border-white">
 				<div className="inline-block text-center">
 					<ImQuotesLeft className="inline relative -top-2 text-[#dd0201] mx-1" />
 					<span>{Hiquote}</span>
@@ -52,12 +50,12 @@ const QuoteGenerator = () => {
 				<div className="flex justify-between items-center mt-10">
 					<button
 						onClick={() => navigate("/")}
-						className="px-4 py-2 rounded-md hover:bg-[#e75917] bg-[#ff5001] text-white">
+						className="px-4 py-2 text-sm md:text-base rounded-md hover:bg-[#e75917] bg-[#ff5001] text-white">
 						Back to Home
 					</button>
 					<button
 						onClick={nextQuote}
-						className="px-4 py-2 rounded-md hover:bg-[#d5323d] bg-[#f7414c] text-white">
+						className="px-4 py-2 text-sm md:text-base rounded-md hover:bg-[#d5323d] bg-[#f7414c] text-white">
 						Next Quote
 					</button>
 				</div>
